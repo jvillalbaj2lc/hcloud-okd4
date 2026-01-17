6fbd4779a8b160f8e3fea3ecb8cbec2d67283635
Joseph Villalba <1817323+jvillalbaj2lc@users.noreply.github.com>
2026-01-17 23:19:55 +0000

    Relax hcloud image selector

--- terraform/image.tf
+++ terraform/image.tf
@@ -1,5 +1,5 @@
 data "hcloud_image" "image" {
-  with_selector = "os=${var.image},image_type=generic,arch=${var.arch}"
+  with_selector = "os=${var.image},arch=${var.arch}"
   with_status   = ["available"]
   most_recent   = true
 }
