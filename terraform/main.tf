b26a9fa34d1b615446edf82e2387ac64d4ab7f67
Joseph Villalba <1817323+jvillalbaj2lc@users.noreply.github.com>
2026-01-26 22:06:40 +0000

    Use ignition IPs when DNS not managed

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,6 +37,15 @@ module "dns-server" {
   network_id     = hcloud_network.network.id
 }
 
+locals {
+  ignition_private_ip = var.bootstrap == true ? module.ignition.private_ipv4_addresses[0] : null
+  ignition_bootstrap_url = var.bootstrap == true ? (
+    var.manage_dns ? "http://ignition.${var.dns_domain}/bootstrap.ign" : "http://${local.ignition_private_ip}/bootstrap.ign"
+  ) : ""
+  ignition_master_url = var.manage_dns ? "https://api-int.${var.dns_domain}:22623/config/master" : "https://${hcloud_load_balancer_network.lb_network.ip}:22623/config/master"
+  ignition_worker_url = var.manage_dns ? "https://api-int.${var.dns_domain}:22623/config/worker" : "https://${hcloud_load_balancer_network.lb_network.ip}:22623/config/worker"
+}
+
 module "bootstrap" {
   source          = "./modules/hcloud_coreos"
   instance_count  = var.bootstrap == true ? 1 : 0
@@ -48,7 +57,7 @@ module "bootstrap" {
   image_name      = var.image
   server_type     = var.server_type_bootstrap
   subnet          = hcloud_network_subnet.subnet.id
-  ignition_url    = var.bootstrap == true ? "http://ignition.${var.dns_domain}/bootstrap.ign" : ""
+  ignition_url    = local.ignition_bootstrap_url
   dns_server_ip   = var.ip_dns_server
   depends_on = [
     module.dns-server
@@ -71,7 +80,7 @@ module "master" {
     "cluster"                   = var.dns_domain
   }
   subnet          = hcloud_network_subnet.subnet.id
-  ignition_url    = "https://api-int.${var.dns_domain}:22623/config/master"
+  ignition_url    = local.ignition_master_url
   ignition_cacert = local.ignition_master_cacert
   dns_server_ip   = var.ip_dns_server
   depends_on = [
@@ -95,7 +104,7 @@ module "worker" {
     "cluster"                   = var.dns_domain
   }
   subnet          = hcloud_network_subnet.subnet.id
-  ignition_url    = "https://api-int.${var.dns_domain}:22623/config/worker"
+  ignition_url    = local.ignition_worker_url
   ignition_cacert = local.ignition_worker_cacert
   dns_server_ip   = var.ip_dns_server
   depends_on = [
@@ -110,6 +119,6 @@ module "hcloud_dns" {
   cluster_domain      = var.dns_domain
   lb_private_ip       = hcloud_load_balancer_network.lb_network.ip
   master_private_ips  = module.master.private_ipv4_addresses
-  ignition_private_ip = var.bootstrap == true ? module.ignition.private_ipv4_addresses[0] : null
+  ignition_private_ip = local.ignition_private_ip
   bootstrap_enabled   = var.bootstrap
 }
